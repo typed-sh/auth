9cebb8af80c281eb08750b4ac0ead4245fb1cce5
HoJeong Go <seia@outlook.kr>
2023-12-25 08:44:26 +0900

    chore: remove tests

--- packages/server/test/index.ts
+++ packages/server/test/index.ts
@@ -1,17 +1,2 @@
 import test from 'ava';
 import {createServer} from '../src';
-
-test('post: /api/onsite/user', async t => {
-	const server = await createServer();
-	const response = await server.inject({
-		url: '/api/onsite/user',
-		method: 'post',
-		body: {
-			email: 'user@domain.tld',
-			password: 'thisissamplepassword!',
-			username: 'user',
-		},
-	});
-
-	t.is(response.body, '');
-});
